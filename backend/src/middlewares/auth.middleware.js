b2a46015ef1ce66d6781c1778c21379dd0c3dda6
Renanppcosta <renanppcosta@gmail.com>
2024-05-28 18:56:21 -0300

    feat authentication process using created middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) =>{
+    const token = req.headers.authorization;
+
+    if(!token){
+        return res.status(401).send("Não autorizado.")
+    }
+
+    try {
+        const replace = token.replace("Bearer ", "");
+        jwt.verify(replace, process.env.JWT_SECRET, (error, decoded)=>{
+            console.log(decoded);
+        });
+        next();
+    } catch (error) {
+        return res.status(401).send({message:"Credenciais inválidas."});
+    }
+}
--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
+
 userRouter.post("/", userController.createUserController);
+
+userRouter.use(authMiddleware);
 userRouter.get("/", userController.getAllUserController);
 userRouter.get("/:id", userController.getUserByIdController);
 userRouter.patch("/:id", userController.updateUserController);
