84952f3a82d2304a6e3bf21d748b6b26effc0a38
Renanppcosta <renanppcosta@gmail.com>
2024-07-22 15:17:31 -0300

    feat: added authentication in expense router

--- backend/src/routes/expense.router.js
+++ backend/src/routes/expense.router.js
@@ -4,6 +4,8 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const expenseRouter = Router();
 
+expenseRouter.use(authMiddleware);
+
 expenseRouter.post("/", expenseController.createExpenseController);
 expenseRouter.get("/", expenseController.getAllExpenseController);
 expenseRouter.patch("/:id", expenseController.updateExpenseController);
